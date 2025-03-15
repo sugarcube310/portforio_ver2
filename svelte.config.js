e0848e7b4e04bade559eba4c87ce8223b1dadf8a
DESKTOP-UUVO7JG\sato <aimkitsou.014@gmail.com>
2025-03-16 01:20:02 +0900

    svelte.config.jsonの変更を戻す

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
